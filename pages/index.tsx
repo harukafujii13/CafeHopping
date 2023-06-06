670e281097612be40ece6d279a85722993eb51b1
Haruka <hfujii1013@gmail.com>
2023-06-06 16:40:46 -0700

    styled nav-icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,37 +9,37 @@ import {
 
 function Homepage() {
   return (
-    <div>
-      <ul>
-        <li>
+    <div className="h-[100vh] bg-light-green">
+      <ul className="flex flex-row justify-end text-primary-gray bg-primary-rose h-[6%] items-center px-10">
+        <li className="ml-5">
           <Link href={"/search"}>
             <FontAwesomeIcon
               icon={faMagnifyingGlass}
-              className="h-[25px]"
+              className="h-[27px]"
             />
           </Link>
         </li>
-        <li>
+        <li className="ml-5">
           <Link href={"/bookmark"}>
             <FontAwesomeIcon
               icon={faBookmark}
-              className="h-[25px]"
+              className="h-[27px]"
             />
           </Link>
         </li>
-        <li>
+        <li className="ml-5">
           <Link href={"/favorite"}>
             <FontAwesomeIcon
               icon={faHeart}
-              className="h-[25px]"
+              className="h-[27px]"
             />
           </Link>
         </li>
-        <li>
+        <li className="ml-5">
           <Link href={"/logout"}>
             <FontAwesomeIcon
               icon={faArrowRightFromBracket}
-              className="h-[25px]"
+              className="h-[27px]"
             />
           </Link>
         </li>
